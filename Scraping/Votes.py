6c7725058a18d643cffaac28a5b387989285df10
Sefi Erlich <erlichsefi@gmail.com>
2021-04-24 22:46:00 +0300

    first working version

--- Scraping/Votes.py
+++ Scraping/Votes.py
@@ -1,104 +1,93 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
+import os,time
 import requests
 import numpy as np
 import pandas as pd
 import lxml.html as LH
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from urllib.parse import urlparse,urlunparse,urlencode
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 base_url = "https://www.knesset.gov.il/vote/heb/Vote_Res_Map.asp"
 search_url = "https://www.knesset.gov.il/vote/heb/vote_search.asp"
 
-headers = {
-    'accept': "*/*",
-    'Accept-Language': "en-US,en;q=0.9,he;q=0.8",
-    'Content-Type': "text/html; charset=utf-8",
-    'Postman-Token': "bf182af5-4500-40d5-ad15-3c134a888bac",
-    'User-Agent': "PostmanRuntime/7.23.0",
-    'Cache-Control': "no-cache",
-    'Host': "www.knesset.gov.il",
-    'Cookie': "rbzid=3pd95R7URU86rXpLcSW6Ra5y7R+6rQJd+Zq4GzfBsQmN4IWmOGdJHGo8SHGdXWXQ4xWHDwF6VfujpVG2lYrjsO1n9SmX+qxSAzRJBFpLbP4=",
-    'Accept-Encoding': "gzip, deflate",
-    'Connection': "keep-alive",
-    'cache-control': "no-cache"
-}
+
+def scrap_table(driver, title):
+    names = list()
+    parties = list()
+    try:
+        against_table = driver.find_element_by_xpath(f"//*[contains(text(),'{title}')]")
+        against_table = against_table.find_elements(By.XPATH, "./../..")[0]
+        
+        for tr in against_table.find_elements(By.TAG_NAME,"tr")[2:-1]:
+                names.append(tr.find_elements(By.TAG_NAME,"td")[1].text)
+                parties.append(tr.find_elements(By.TAG_NAME,"td")[2].text)
+    except NoSuchElementException:
+        pass
+
+    return names,parties
+
+def is_empty(x):
+    return len(x) == 3 and x.count(" ") == 2 and x.count('"') == 1
 
 latest_rule_id = 33461
-
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument("disable-blink-features=AutomationControlled")
+driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
+url = urlparse(base_url)
 for rule_id in range(latest_rule_id, 0, -1):
     
-    query_string = {"vote_id_t": str(rule_id)}    
-    response = requests.request("GET", base_url, headers=headers, params=query_string)
-    root = LH.fromstring(response.content)
-       
-    if response.url != search_url:
-        
-        in_favor_name = [x.text_content() for x in
-                         root.xpath('//tr[3]/td[2]/table/tr[2]/td/table[2]/tr/td[1]/table[1]/tr[2]/td/table/tr/td[2]') if
-                         x.text_content() != '']
+    query_string = urlencode({"vote_id_t": str(rule_id)})   
+    url_parse = url._replace(query=query_string)
+    new_url = urlunparse(url_parse)
+    driver.get(new_url)
+    
+    time.sleep(5)
 
-        in_favor_party = [x.text_content() for x in
-                          root.xpath('//tr[3]/td[2]/table/tr[2]/td/table[2]/tr/td[1]/table[1]/tr[2]/td/table/tr/td[3]') if
-                          x.text_content() != '']
+    in_favor_name, in_favor_party = scrap_table(driver, 'שהצביעו בעד')
+    against_name, against_party = scrap_table(driver, 'שהצביעו נגד')
+    avoid_name, avoid_party = scrap_table(driver, 'שהצביעו שנמנעו')
 
-        against_name = [x.text_content() for x in
-                        root.xpath('//tr[3]/td[2]/table/tr[2]/td/table[2]/tr/td[2]/table[1]/tr[2]/td/table/tr/td[2]') if
-                        x.text_content() != '']
+    committee_num = driver.find_element_by_xpath(f"//*[contains(text(),'מספר הצבעה')]/./../td[2]").text
 
-        against_party = [x.text_content() for x in
-                         root.xpath('//tr[3]/td[2]/table/tr[2]/td/table[2]/tr/td[2]/table[1]/tr[2]/td/table/tr/td[3]') if
-                         x.text_content() != '']
+    meeting_num = driver.find_element_by_xpath(f"//*[contains(text(),'מספר ישיבה')]/./../td[2]").text
 
-        avoid_name = [x.text_content() for x in
-                      root.xpath('//tr[3]/td[2]/table/tr[2]/td/table[2]/tr/td[1]/table[2]/tr[2]/td/table/tr/td[2]') if
-                      x.text_content() != '']
+    date = driver.find_element_by_xpath(f"//*[contains(text(),'תאריך')]/./../td[2]").text
 
-        avoid_party = [x.text_content() for x in
-                       root.xpath('//tr[3]/td[2]/table/tr[2]/td/table[2]/tr/td[1]/table[2]/tr[2]/td/table/tr/td[3]') if
-                       x.text_content() != '']
+    rule = driver.find_element_by_xpath(f"//*[contains(text(),'שם החוק')]/./../td[2]").text
 
-        no_vote_name = [x.text_content() for x in
-                      root.xpath('//tr[3]/td[2]/table/tr[2]/td/table[2]/tr/td[2]/table[2]/tr[2]/td/table/tr/td[2]') if
-                      x.text_content() != '']
+    head_name = driver.find_element_by_xpath(f"//*[contains(text(),'יושב ראש')]/./../td[2]").text
 
-        no_vote_party = [x.text_content() for x in
-                       root.xpath('//tr[3]/td[2]/table/tr[2]/td/table[2]/tr/td[2]/table[2]/tr[2]/td/table/tr/td[3]') if
-                       x.text_content() != '']
+    in_favor_df = pd.DataFrame(list(zip(in_favor_name, in_favor_party))[1:], columns=['Name', 'Party'])
+    in_favor_df['Vote'] = 'בעד'
 
-        committee = root.xpath('//tr[3]/td[2]/table/tr[2]/td/center/table/tr[1]/td[2]')[0].text_content()
+    against_df = pd.DataFrame(list(zip(against_name, against_party))[1:], columns=['Name', 'Party'])
+    against_df['Vote'] = 'נגד'
 
-        meeting = root.xpath('//tr[3]/td[2]/table/tr[2]/td/center/table/tr[2]/td[2]')[0].text_content()
+    avoid_df = pd.DataFrame(list(zip(avoid_name, avoid_party))[1:], columns=['Name', 'Party'], )
+    avoid_df['Vote'] = 'נמנע'
 
-        date = root.xpath('//tr[3]/td[2]/table/tr[2]/td/center/table/tr[3]/td[2]')[0].text_content()
+    dataframes = [in_favor_df, against_df, avoid_df]
 
-        rule = root.xpath('//tr[3]/td[2]/table/tr[2]/td/center/table/tr[4]/td[2]')[0].text_content()
+    for df in dataframes:
+        df["Party"] = df["Party"].apply( lambda x: np.nan if is_empty(x) else x)
+        df.fillna(method='ffill', inplace=True)
+        df['Rule Name'] = rule
+        df['Meeting Date'] = date
+        df['Committee Leader Name'] = head_name
+        df['Vote Number'] = committee_num
+        df['Meeting Number'] = meeting_num
+        df['Id'] = 'https://www.knesset.gov.il/vote/heb/Vote_Res_Map.asp?vote_id_t='+str(rule_id)
 
+    current_path = os.getcwd()
 
-        in_favor_df = pd.DataFrame(list(zip(in_favor_name, in_favor_party))[1:], columns=['Name', 'Party'])
-        in_favor_df['Vote'] = 'בעד'
-
-        against_df = pd.DataFrame(list(zip(against_name, against_party))[1:], columns=['Name', 'Party'])
-        against_df['Vote'] = 'נגד'
-
-        avoid_df = pd.DataFrame(list(zip(avoid_name, avoid_party))[1:], columns=['Name', 'Party'], )
-        avoid_df['Vote'] = 'נמנע'
-
-        no_vote_df = pd.DataFrame(list(zip(no_vote_name, no_vote_party))[1:], columns=['Name', 'Party'], )
-        no_vote_df['Vote'] = 'לא הצביע'
-
-        dataframes = [in_favor_df, against_df, avoid_df, no_vote_df]
-
-        for df in dataframes:
-            df.replace(' " ', np.nan, inplace=True)
-            df.fillna(method='ffill', inplace=True)
-            df['Rule'] = rule
-            df['Date'] = date
-            df['Committee'] = committee
-            df['Meeting'] = meeting
-            df['Id'] = 'https://www.knesset.gov.il/vote/heb/Vote_Res_Map.asp?vote_id_t='+str(rule_id)
-
-        current_path = os.getcwd()
-
-        pd.concat(dataframes).to_csv(f'{current_path}/{rule_id}.csv', encoding='utf-8-sig', index=False)
-        print("downloaded data for rule id: ", rule_id)
+    final_df = pd.concat(dataframes)
+    
+    final_df.to_csv(f'{current_path}/{rule_id}.csv', encoding='utf-8-sig', index=False)
+    print("downloaded data for rule id: ", rule_id)
